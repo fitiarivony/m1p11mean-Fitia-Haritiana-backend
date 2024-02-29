bc9ada95af2b0b9bc0402408fbb86591ee5e5b08
fitiarivony <92774199+fitiarivony@users.noreply.github.com>
2024-02-29 15:08:49 +0300

    Update token.js

--- models/token.js
+++ models/token.js
@@ -37,13 +37,13 @@ tokenSchema.methods={
         const Token=mongoose.model('Token',tokenSchema);
         if (!authorization) {
             console.log("Authentication tsisy");
-            throw new Error('No credentials provided');
+            throw new Error("Votre connexion a epxiré, veuillez vous reconnecter");
       }
       let token_hash=authorization.split(' ')[1];
       let now=new Date();
       let token= await Token.findOne({token: token_hash,date_expiration:{$gte:(now) }});
       console.log(token_hash,token);
-      if (!token || token ==null) throw new Error('No credentials provided');
+      if (!token || token ==null) throw new Error("Votre connexion a epxiré, veuillez vous reconnecter");
       return token;
     },
    
@@ -52,26 +52,26 @@ tokenSchema.statics.authenticate=async function(authorization,statut){
     const Token=mongoose.model('Token',tokenSchema);
     if (!authorization) {
         console.log("Authentication tsisy");
-        throw new Error('No credentials provided');
+        throw new Error("Votre connexion a epxiré, veuillez vous reconnecter");
   }
   let token_hash=authorization.split(' ')[1];
   let now=new Date();
   let token= await Token.findOne({token: token_hash,date_expiration:{$gte:(now) },statut:statut});
   console.log(token_hash,token);
-  if (!token || token ==null) throw new Error('No credentials provided');
+  if (!token || token ==null) throw new Error("Votre connexion a epxiré, veuillez vous reconnecter");
   return token;
 }
 tokenSchema.statics.authenticateAll=async function(authorization){
     const Token=mongoose.model('Token',tokenSchema);
     if (!authorization) {
         console.log("Authentication tsisy");
-        throw new Error('No credentials provided');
+        throw new Error("Votre connexion a epxiré, veuillez vous reconnecter");
   }
   let token_hash=authorization.split(' ')[1];
   let now=new Date();
   let token= await Token.findOne({token: token_hash,date_expiration:{$gte:(now) }});
   console.log(token_hash,token);
-  if (!token || token ==null) throw new Error('No credentials provided');
+  if (!token || token ==null) throw new Error("Votre connexion a epxiré, veuillez vous reconnecter");
   return token;
 }
 tokenSchema.statics.generateToken=async function(id_admin,statut){
